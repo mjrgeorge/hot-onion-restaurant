f97bcb36b2b0328bea5a7da35a13c7d1ca9eb1a1
mjrgeorge <mjrgeorge@gmail.com>
2020-09-14 15:29:21 +0600

    fake data create and added

---
+++ src/fakeData/FakeData.js
@@ -0,0 +1,147 @@
+const FakeData = [{
+        "id": "1",
+        "price": 10,
+        "category": "breakfast",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/mBJDMFM"
+    },
+    {
+        "id": "2",
+        "price": 12,
+        "category": "breakfast",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/mXhJB0B"
+    },
+    {
+        "id": "3",
+        "price": 15,
+        "category": "breakfast",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/s6LqwdS"
+    },
+    {
+        "id": "4",
+        "price": 11,
+        "category": "breakfast",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/Y7Bgxcs"
+    },
+    {
+        "id": "5",
+        "price": 13,
+        "category": "breakfast",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/9hR40KD"
+    },
+    {
+        "id": "16",
+        "price": 25,
+        "category": "breakfast",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/BgnkVzK"
+    },
+    {
+        "id": "7",
+        "price": 27,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/qRT4Mtr"
+    },
+    {
+        "id": "8",
+        "price": 29,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/tLhWCvJ"
+    },
+    {
+        "id": "9",
+        "price": 26,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/vvQ0mmm"
+    },
+    {
+        "id": "10",
+        "price": 28,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/G9jvn5M"
+    },
+    {
+        "id": "11",
+        "price": 30,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/vvVYMsP"
+    },
+    {
+        "id": "12",
+        "price": 29,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/djWb8RC"
+    },
+    {
+        "id": "13",
+        "price": 40,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/FKbm0QP"
+    },
+    {
+        "id": "14",
+        "price": 43,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/nD4Lyf8"
+    },
+    {
+        "id": "15",
+        "price": 37,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/wQ6G2Z1"
+    },
+    {
+        "id": "16",
+        "price": 35,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/QMS00Wv"
+    },
+    {
+        "id": "17",
+        "price": 32,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/J7Ykx27"
+    },
+    {
+        "id": "18",
+        "price": 48,
+        "category": "lunch",
+        "item": "Lorem Ipsum is simply dummy text.",
+        "description": "Lorem text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+        "image": "https://ibb.co/FqrGzRQ"
+    }
+];
+
+export default FakeData;
